1a0f03b4fde2aa8c5e6a1ffa73f0d91fc4370fe8
KevinWullur <s11810220@student.unklab.ac.id>
2020-11-11 23:13:02 +0800

    first commit

---
+++ pertemuan_20/index.js
@@ -0,0 +1,67 @@
+//variabel
+
+//let & const
+// let _umur = 25;
+// _umur = 26;
+// let nilai = 95.5;
+// let sudahMenikah = true;
+// let $nama = "John Doe";
+// console.log(_umur);
+// console.log($nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir = 2010;
+// console.log(tahunlahir);
+
+//operator
+
+//Arithmetic Operator(+,-,x,/)
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1 + angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("nama saya "namaDepan + " " + namaBelakang ". Umur saya adalah " + umur + " tahun");
+
+//operator perbandingan
+
+// let angka1 = 10;
+// let angka2 = "10";
+
+// console.log(angka1 <= angka2);
+
+//ternary operator
+// let angka = 5;
+// let hasil = angka <= 0 ? "negatif" : "positf";
+// console.log(hasil);
+
+// //operator logika
+// let kondisi1 = 1 < 2;
+// let kondisi2 = false;
+// console.log(kondisi1 && kondisi2);
+
+// console.log(!kondisi1);
+
+// let angka = prompt("Masukan angka :");
+// console.log("angka yang dimasukan adalah" + angka);
+
+
+//Exercise #7
+
+let berat = 3;
+let tinggi = 5;
+
+let angka = prompt("masukan angka :");
+console.log(" = " + angka);
+let hasil =berat/tinggi * tinggi;
+console.log(hasil);
+
+ 
+
+
+
